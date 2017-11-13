42a468b53db02bbf216bad5c7ef4385d98154dea
e4ainst <e4ainst@vdevpril369dsy>
2017-11-13 11:36:16 +0100

    [GDE] Initial commit

---
+++ Sensor_Signal_Filtering_read_sensor.c
@@ -0,0 +1,13 @@
+#define K 0.45
+//filter noise on sensors 
+
+void read_sensor(void){
+	uint16_t new_distance = 0;
+  	static uint16_t distance = 0;
+  
+	RTE_Call_read_distance(&new_distance);
+  	// Remove noise
+  	distance = K * new_distance + (1 - K) * distance;
+  	
+  	RTE_Write_screen_distance(&distance);
+}
