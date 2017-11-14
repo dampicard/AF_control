a7b9feff5eab78a68c37b3d45e6fd4cb11706996
e4ainst <e4ainst@vdevpril369dsy>
2017-11-14 15:26:48 +0100

    [GDE] Modification of runnable read_sensor

--- Sensor_Signal_Filtering_read_sensor.c
+++ Sensor_Signal_Filtering_read_sensor.c
@@ -2,7 +2,7 @@
 //filter noise on sensors 
 
 void read_sensor(void){
-	uint16_t new_distance = 0;
+	uint16_t new_distance = 0
   	static uint16_t distance = 0;
   
 	RTE_Call_read_distance(&new_distance);
