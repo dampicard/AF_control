2ad76f1b53f9a38ab341ee7995a5952a68162889
e4ainst <e4ainst@vdevpril369dsy>
2017-11-14 15:49:47 +0100

    [GDE] Modification of runnable read_switch_status

--- Switch_Button_Driver_read_switch_status.c
+++ Switch_Button_Driver_read_switch_status.c
@@ -1,11 +1,11 @@
-#include <stdint.h>
-#include "Fmi_Cfg.h" // Ids for <XXX>WriteToSharedObj
-#include "ASimSharedObjUtils.h" // <XXX>WriteToSharedObj
+//#include <stdint.h>
+//#include "Fmi_Cfg.h" // Ids for <XXX>WriteToSharedObj
+//#include "ASimSharedObjUtils.h" // <XXX>WriteToSharedObj
 
 void read_switch_status(uint8_t* status){
-  fmi2Integer data;
+/*  fmi2Integer data;
   
   fmi2IntegerReadFromWriteBuffer(InputId_Switch_Button_Driver_read_switch_status_0, &data);
   
-  *status = (uint8_t) data;
+  *status = (uint8_t) data;*/
 }
