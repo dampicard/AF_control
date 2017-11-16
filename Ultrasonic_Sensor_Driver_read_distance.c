a027f97b2fabc54ac89db95f8f1cd837287d8e1d
dampicard <dampicard@users.noreply.github.com>
2017-11-16 17:39:51 +0100

    Update Ultrasonic_Sensor_Driver_read_distance.c

--- Ultrasonic_Sensor_Driver_read_distance.c
+++ Ultrasonic_Sensor_Driver_read_distance.c
@@ -1,10 +1,10 @@
-//#include "Fmi_Cfg.h" // Ids for <XXX>WriteToSharedObj
-//#include "ASimSharedObjUtils.h" // <XXX>WriteToSharedObj
+#include "Fmi_Cfg.h" // Ids for <XXX>WriteToSharedObj
+#include "ASimSharedObjUtils.h" // <XXX>WriteToSharedObj
 
 void read_distance(uint16_t* distance){
- /* fmi2Integer data;
+  fmi2Integer data;
   
   fmi2IntegerReadFromWriteBuffer(InputId_Ultrasonic_Sensor_Driver_read_distance_0, &data);
   
-  *distance = (uint16_t) data;*/
+  *distance = (uint16_t) data;
 }
