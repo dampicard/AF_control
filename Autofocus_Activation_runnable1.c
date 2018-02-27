3e4cb3d3a5f93ccc241c764df82a447ca510ff5b
sdk <damien.picard@3ds.com>
2018-02-27 14:31:59 +0100

    [GDE] Modification of runnable Autofocus_Activation_runnable1.c

--- Autofocus_Activation_runnable1.c
+++ Autofocus_Activation_runnable1.c
@@ -37,6 +37,40 @@
       "width": 45.0,
       "height": 35.0,
       "simuStartIndex": 0
+    },
+    {
+      "name": "Gain2",
+      "type": {
+        "operation": "GAIN",
+        "portSpecifications": [
+          {
+            "name": "1",
+            "direction": "IN",
+            "xRatio": 0.0,
+            "yRatio": 0.5,
+            "simuNumber": 0
+          },
+          {
+            "name": "2",
+            "direction": "OUT",
+            "xRatio": 1.0,
+            "yRatio": 0.5,
+            "simuNumber": 0
+          }
+        ]
+      },
+      "attributes": [
+        {
+          "name": "Gain",
+          "type": "http://www.w3.org/2001/XMLSchema#double",
+          "value": "1"
+        }
+      ],
+      "x": -22.5,
+      "y": -17.5,
+      "width": 45.0,
+      "height": 35.0,
+      "simuStartIndex": 0
     }
   ],
   "ios": [
