fa5f09f1315ccd128a63f8432289b085817659d0
sdk <damien.picard@3ds.com>
2018-02-27 14:32:42 +0100

    [GDE] Initial commit

---
+++ Autofocus_Activation_runnable2.c
@@ -0,0 +1,46 @@
+{
+  "interface_": {
+    "type": "SYSTEM",
+    "portSpecifications": []
+  },
+  "blockStates": [
+    {
+      "isInitial": true,
+      "name": "State1",
+      "type": {
+        "type": "STATE",
+        "portSpecifications": [
+          {
+            "name": "1",
+            "direction": "IN",
+            "xRatio": 0.5,
+            "yRatio": 0.0,
+            "simuNumber": 0
+          },
+          {
+            "name": "2",
+            "direction": "OUT",
+            "xRatio": 0.5,
+            "yRatio": 1.0,
+            "simuNumber": 0
+          },
+          {
+            "name": "Action",
+            "direction": "ACTION",
+            "xRatio": 1.0,
+            "yRatio": 0.5,
+            "simuNumber": 0
+          }
+        ]
+      },
+      "x": -40.0,
+      "y": -40.0,
+      "width": 80.0,
+      "height": 80.0,
+      "simuStartIndex": 0
+    }
+  ],
+  "blockActions": [],
+  "blockTransitions": [],
+  "connections": []
+}
