65c79622527398ae311a684c76ea01ee61ac5a55
sdk <damien.picard@3ds.com>
2018-02-27 14:32:45 +0100

    [GDE] Modification of runnable Autofocus_Activation_runnable2.c

--- Autofocus_Activation_runnable2.c
+++ Autofocus_Activation_runnable2.c
@@ -41,6 +41,35 @@
     }
   ],
   "blockActions": [],
-  "blockTransitions": [],
+  "blockTransitions": [
+    {
+      "condition": "True",
+      "name": "Transition1",
+      "type": {
+        "type": "TRANSITION",
+        "portSpecifications": [
+          {
+            "name": "1",
+            "direction": "IN",
+            "xRatio": 0.5,
+            "yRatio": 0.0,
+            "simuNumber": 0
+          },
+          {
+            "name": "2",
+            "direction": "OUT",
+            "xRatio": 0.5,
+            "yRatio": 1.0,
+            "simuNumber": 0
+          }
+        ]
+      },
+      "x": -26.666666,
+      "y": -2.5,
+      "width": 53.333332,
+      "height": 5.0,
+      "simuStartIndex": 0
+    }
+  ],
   "connections": []
 }
