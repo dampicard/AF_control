68f7b25c07ccd61fe58970d474a14d3d7e0df636
sdk <A56@LP5-A56-CEM.dsone.3ds.com>
2018-03-16 15:00:43 +0100

    [GDE] Modification of runnable Servo_Motor_Driver_set_motor_angle.c

--- Servo_Motor_Driver_set_motor_angle.c
+++ Servo_Motor_Driver_set_motor_angle.c
@@ -4,5 +4,5 @@
 // IN
 void set_motor_angle(uint16_t angle){
   	fmi2Integer buf = (fmi2Integer) angle;
-  	fmi2IntegerWriteToSharedObj(SharedId_Servo_Motor_Driver_set_motor_angle_0, &buf);
+  	fmi2IntegerWriteToSharedObj(SharedId_Servo_Motor_Driver_set_motor_angle_angle, &buf);
 }
